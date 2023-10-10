750bfb3429b7baa189be7eb40d1bcb4d23a05050
TheGaurav123 <iamakashgaurav321@gmail.com>
2023-10-10 13:59:13 +0530

    Allowed All

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ async function init() {
 
     const app = express() 
     app.use(cors({
-        origin: process.env.FRONTEND_URL
+        origin: "*"
     }))
     app.use(express.json({ limit: '500mb' }))
     
